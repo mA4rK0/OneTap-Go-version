23d0491752bcaf27e22ec961c0df969be89a04aa
mA4rK0 <onlymarko25@gmail.com>
2025-10-14 10:15:09 +0700

    feat: first step

---
+++ BackEnd/models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	InternalID uint64 `json:"internal_id" db:"internal_id" gorm:"primaryKey"`
+	PublicID   uuid.UUID `json:"public_id" db:"public_id"`
+	Name       string `json:"name" db:"name"`
+	Email      string `json:"email" db:"email" gorm:"unique"`
+	Password   string `json:"password" db:"password" gorm:"column:password"`
+	CreatedAt  time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time	`json:"updated_at" db:"updated_at"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"index"` // "soft" delete in database, cannot return deleted data
+}
